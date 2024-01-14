7e9048aa7328a6eaa3f3bfaa2fab1e8513fe3730
Volodymyr L <northwoodzp@gmail.com>
2024-01-14 11:54:10 +0200

    header and footer added

--- projects/forms-playground/src/app/app.component.ts
+++ projects/forms-playground/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ projects/forms-playground/src/app/core/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+  public get year() {
+    return new Date().getFullYear();
+  }
+
+}
